6020de7365f1bd823eeb8e348309c812e443677d
Robert Amadeo Yuwono <robert@Roberts-MacBook-Air.local>
2024-11-08 23:21:30 +0700

    Updated .gitignore, main.dart, and pubspec files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:notes_but_better/firebase_options.dart';
 import 'package:notes_but_better/pages/loginpage.dart';
 import 'package:notes_but_better/pages/registerpage.dart';
@@ -10,12 +11,21 @@ import 'package:notes_but_better/change_notifiers/new_note_controller.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load();
+  // Retrieve API keys here
+  final apiKey1 = dotenv.env['GOOGLE_API_KEY1'];
+  final apiKey2 = dotenv.env['GOOGLE_API_KEY2'];
+  final apiKey3 = dotenv.env['GOOGLE_API_KEY3'];
+
   try {
     // Initialize Firebase and log success
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    print('Firebase successfully initialized.');
+    // print('Firebase successfully initialized.');
+    // print('API Key 1: $apiKey1');
+    // print('API Key 2: $apiKey2');
+    // print('API Key 3: $apiKey3');
   } catch (e) {
     // Log the error if Firebase initialization fails
     print('Error initializing Firebase: $e');
